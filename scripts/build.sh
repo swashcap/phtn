55b4ef635f462f94a2135e93d867a4ac34e111f2
Cory Reed <swashcap@gmail.com>
2019-02-17 12:16:58 -0800

    feat: build script

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir -p dist
+
+cat << EOF > dist/phtn.css
+/**
+ * phtn: A hastily composed library of utility CSS classes.
+ * v$(jq -r .version package.json)
+ *
+ * Homepage: https://github.com/swashcap/phtn
+ * License: MIT (https://github.com/swashcap/phtn/blob/master/LICENSE)
+ * Author: Cory Reed (https://github.com/swashcap)
+ */
+
+EOF
+
+cat src/{variables,element,layout,spacing,text}.css >> dist/phtn.css
+
+cat dist/phtn.css | node_modules/.bin/csso > dist/phtn.min.css

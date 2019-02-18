764a37427a9b9578a21fc4b57e2b425b25b9d061
Cory Reed <swashcap@gmail.com>
2019-02-17 17:11:39 -0800

    feat: add docs script

---
+++ scripts/docs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eo pipefail
+
+node_modules/.bin/kss --css kss-assets/phtn.min.css --destination docs \
+  --homepage ../README.md --source src
+
+cp dist/phtn.min.css docs/kss-assets/

6a269e530138e3542d3595dc7fe1d569ee2bcb48
Cory Reed <swashcap@gmail.com>
2019-02-18 10:55:56 -0800

    fix: change sed command based on CLI flavor

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -eo pipefail
 
-sed -i='' "s/__PHTN_VERSION__/$1/; s/__PHTN_PUBLISHED__/$2/" dist/*.css
+REPLACE="s/__PHTN_VERSION__/$1/; s/__PHTN_PUBLISHED__/$2/"
+
+# GNU sed doesn't like the BSD-style `-i` flag:
+if [ "$(command -v sed)" = /bin/sed ]; then
+  sed -i "$REPLACE" dist/*.css
+else
+  sed -i "" "$REPLACE" dist/*.css
+fi
 
